c957e31cfb24552abc4828f9d50615c196cac5d9
yh1 <775474915@qq.com>
2019-10-17 18:28:46 +0800

    USE_SQL

--- server/server_plus.py
+++ server/server_plus.py
@@ -2,65 +2,62 @@ import config
 import parse
 import pymysql
 import os
-from requests_toolbelt.multipart import decoder
 from flask import Flask
-from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
 
 #init connection
 app = Flask(__name__)
-app.config.from_object(config)
-db = SQLAlchemy(app)
+if config.USE_MYSQL:
+    app.config.from_object(config)
+    db = SQLAlchemy(app)
 
 # test connection
 MAGIC_CONNECTION_KEY = 'SYSU_SMC'
 # the absolute path of mediatype file
 MEDIATYPE_FILE_PATH = os.path.abspath('.') + "/user_data/"
 
+if config.USE_MYSQL:
+    #text
+    class File(db.Model):
+        __tablename__ = 'File'
+        fid = db.Column(db.Integer, primary_key = True, autoincrement=True)
+        name = db.Column(db.String(30),nullable=False)
+        date = db.Column(db.String(50), nullable=False)
+        latitude = db.Column(db.Float,nullable=False)
+        longitude = db.Column(db.Float,nullable=False)
+        accelerometer_x = db.Column(db.Float,nullable=False)
+        accelerometer_y = db.Column(db.Float,nullable=False)
+        accelerometer_z = db.Column(db.Float,nullable=False)
 
-class File(db.Model):
-    __tablename__ = 'File'
-    fid = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    name = db.Column(db.String(30),nullable=False)
-    date = db.Column(db.String(50), nullable=False)
-    latitude = db.Column(db.Float,nullable=False)
-    longitude = db.Column(db.Float,nullable=False)
-    accelerometer_x = db.Column(db.Float,nullable=False)
-    accelerometer_y = db.Column(db.Float,nullable=False)
-    accelerometer_z = db.Column(db.Float,nullable=False)
 
+        def __init__(self, name, date, latitude, longitude, accelerometer_x, accelerometer_y, accelerometer_z):
+            self.name = name
+            self.date = date
+            self.latitude = latitude
+            self.longitude = longitude
+            self.accelerometer_x = accelerometer_x
+            self.accelerometer_y = accelerometer_y
+            self.accelerometer_z = accelerometer_z
+        
+        def __repr__(self):
+            return '<File %r>' % self.name
 
-    def __init__(self, name, date, latitude, longitude, accelerometer_x, accelerometer_y, accelerometer_z):
-        self.name = name
-        self.date = date
-        self.latitude = latitude
-        self.longitude = longitude
-        self.accelerometer_x = accelerometer_x
-        self.accelerometer_y = accelerometer_y
-        self.accelerometer_z = accelerometer_z
-    
-    def __repr__(self):
-        return '<File %r>' % self.name
+    #Mediatype
+    class Media(db.Model):
+        __tablename__ = 'Media'
+        Mid = db.Column(db.Integer, primary_key = True, autoincrement=True)
+        name = db.Column(db.String(50),nullable=False)
+        Uri = db.Column(db.String(100),nullable=False)
 
-class Media(db.Model):
-    __tablename__ = 'Media'
-    Mid = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    name = db.Column(db.String(50),nullable=False)
-    Uri = db.Column(db.String(100),nullable=False)
+        def __init__(self, name, Uri):
+            self.name = name
+            self.Uri = Uri
+        
+        def __repr__(self):
+            return '<Media %r>' % self.name
 
-    def __init__(self, name, Uri):
-        self.name = name
-        self.Uri = Uri
-    
-    def __repr__(self):
-        return '<Media %r>' % self.name
-
-    
-#test connection
-@app.route('/index', methods=['GET'])
-def test():
-    return "hahahahahaha"
+    db.create_all()
 
 @app.route('/', methods=['GET','POST'])
 def upload():
@@ -71,6 +68,11 @@ def upload():
         if file is None:
             return 'file is none!'
         
+        if not config.USE_MYSQL:
+            file_path = MEDIATYPE_FILE_PATH + file.filename
+            file.save(file_path)
+            return "upload success!"
+        
         #mediatype file 
         if file.filename.find('DataCollectUtils') == -1:
             file_path = MEDIATYPE_FILE_PATH + file.filename
@@ -78,7 +80,8 @@ def upload():
             
             m = Media(file.filename, file_path)
             db.session.add(m)
-        
+
+        #text file parse
         if file.filename.find('DataCollectUtils') != -1:
             f_data = file.read()
             date_arr, location_arr, ac_arr = parse.get_contends_arr(f_data)
@@ -102,6 +105,7 @@ def upload():
         db.session.commit()
         return "upload success!"
 
+    #test connection
     if request.method == 'GET':
         print(request.values)
         if request.values['magicKey'] == MAGIC_CONNECTION_KEY:
@@ -109,5 +113,4 @@ def upload():
             return MAGIC_CONNECTION_KEY
 
 if __name__ == '__main__': 
-    db.create_all()
     app.run(host='0.0.0.0', debug=True)
